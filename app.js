05f2319c3c951931f4b3b497174fe4b06de122f4
directory-glitch <directory-glitch@users.noreply.github.com>
2026-04-06 17:31:41 +0900

    Update DB: Apply new Webhook URL to frontend

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ function initApplication() {
 
   // ============== DATA TRACKING LOGIC (Webhooks / Google Sheets) ==============
   // 정적 호스팅(가비아)에서는 DB가 없으므로 Make(Integromat), Zapier 또는 구글 앱스 스크립트 웹훅 URL로 데이터를 쏩니다.
-  const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzVRecGgHCWpWYJrskDsgt3f2vYg3W9rkEUhjImo5T_AXLtGwzpraZmvikzY8lIRiuYwA/exec";
+  const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxcxBOu880nUnwatysofwpfMAg4Kpk9TpgHsp1q4jXX18zddBgi9z3-av8dVFP5GuKNRQ/exec";
 
   let userLocation = null;
 
